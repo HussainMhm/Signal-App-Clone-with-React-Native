d6e667c06106e52e7b2e31ccdd7306dc5a2f4fa5
hussain <hussainabdullatif@marun.edu.tr>
2023-12-07 23:05:20 +0300

    Implemented add new chat feature

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,59 @@
+import React, { useLayoutEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, Input } from "@rneui/base";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const AddChatScreen = () => {
+import { collection, addDoc } from "firebase/firestore";
+import { FIREBASE_DB } from "../firebase";
+
+const AddChatScreen = ({ navigation }) => {
+    const [input, setInput] = useState("");
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Add a new Chat",
+            headerBackTitle: "Chats",
+        });
+    }, [navigation]);
+
+    async function createChatHandler() {
+        await addDoc(collection(FIREBASE_DB, "chats"), {
+            chatName: input,
+        })
+            .then(() => {
+                navigation.goBack();
+            })
+            .catch((error) => alert(error.message));
+    }
+
     return (
-        <View>
-            <Text>Add Chat Screen</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder="Enter a chat name"
+                value={input}
+                autoFocus
+                onSubmitEditing={createChatHandler}
+                onChangeText={(text) => setInput(text)}
+                leftIcon={
+                    <Icon
+                        name="wechat"
+                        type="antdesign"
+                        size={24}
+                        color="black"
+                        style={{ marginLeft: 10, marginRight: 10 }}
+                    />
+                }
+            />
+            <Button disabled={!input} title="Create new Chat" onPress={createChatHandler} />
         </View>
     );
 };
 
 export default AddChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        padding: 30,
+    },
+});
