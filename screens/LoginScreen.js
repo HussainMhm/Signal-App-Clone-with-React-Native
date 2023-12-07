12b56b9b1f1fa7766cde69017fe9e621e9cfc989
hussain <hussainabdullatif@marun.edu.tr>
2023-12-07 20:35:24 +0300

    Designed the login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "@rneui/base";
+import { StatusBar } from "expo-status-bar";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function signInHandler() {
+        console.log("Sign in");
+    }
+
     return (
-        <View>
-            <Text>Login Screen</Text>
-        </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Signal-Logo.svg/1200px-Signal-Logo.svg.png",
+                }}
+                style={{ width: 200, height: 200, borderRadius: 20, margin: 20 }}
+            />
+
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Email"
+                    autoFocus
+                    type="email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input
+                    placeholder="Password"
+                    secureTextEntry
+                    type="password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+            </View>
+
+            <Button containerStyle={styles.button} onPress={signInHandler} title="Login" />
+            <Button
+                containerStyle={styles.button}
+                onPress={() => navigation.navigate("Register")}
+                type="outline"
+                title="Register"
+            />
+
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
     );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    inputContainer: {
+        marginTop: 10,
+        width: 300,
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+});
